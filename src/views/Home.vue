67f9a2900b8baec8d6b777146248906cffb8614b
Jarosław Nieściór <jaroslaw.niescior@binarapps.com>
2021-12-30 19:17:35 +0100

    feat: add ByronDexModule, ByronWalletModule, Team components to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="home">
     <Hero/>
+    <ByronDexModule/>
+    <ByronWalletModule/>
     <Roadmap/>
     <Financing/>
+    <Team/>
     <Community/>
   </div>
 </template>
@@ -12,13 +15,19 @@ import Hero from "@/components/Hero.vue";
 import Roadmap from "@/components/Roadmap.vue";
 import Financing from "@/components/Financing.vue"
 import Community from "@/components/Community.vue";
+import ByronDexModule from "@/components/ByronDexModule.vue";
+import ByronWalletModule from "@/components/ByronWalletModule.vue";
+import Team from "@/components/Team.vue";
 
 export default {
   name: 'Home',
   components: {
     Hero,
+    ByronDexModule,
+    ByronWalletModule,
     Roadmap,
     Financing,
+    Team,
     Community
   }
 }
