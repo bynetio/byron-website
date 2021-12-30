a360082342db205197d5b0f6ecb9b83bad680cd3
Jarosław Nieściór <jaroslaw.niescior@binarapps.com>
2021-12-30 12:28:17 +0100

    ref: temporary remove unused router links

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,37 +1,21 @@
 <template>
   <nav id="nav" class="navbar container d-flex flex-wrap ">
     <router-link class="nav-item main-item home float-left" to="/"
-      ><span class="home-logo"><img alt="" src="../assets/logo_byron.png"/></span
+      ><span class="home-logo"
+        ><img alt="" src="../assets/logo_byron.png"/></span
     ></router-link>
-    <router-link class="nav-item p-2" to="/solutions"></router-link>
-    <router-link class="nav-item p-2" to="/developers"></router-link>
-    <router-link class="nav-item p-2" to="/use-case"></router-link>
-    <router-link class="nav-item p-2" to="/about"></router-link>
-    <a
-      class="btn btn-outline-primary p-2"
-      target="_blank"
-      href="/static/byron-network-lightpaper.pdf"
-      >Lightpaper</a
-    >
-    <a
-      class="btn btn-outline-primary p-2"
-      target="_blank"
-      href="/static/byron-network-whitepaper.pdf"
-      >Whitepaper</a
-    >
   </nav>
 </template>
 
 <script>
 export default {
   name: 'TopBar',
-  components: {},
 };
 </script>
 
 <style lang="scss">
 #nav {
-  padding: 30px 0;
+  padding: 30px 0 0;
 
   .main-item {
     &:hover {
@@ -47,9 +31,7 @@ export default {
     color: #6d6f8d;
     font-size: 15px;
     font-weight: 500;
-    margin-left: 60px;
     width: 80vw;
-    margin: auto;
 
     &:hover {
       text-decoration: none;
