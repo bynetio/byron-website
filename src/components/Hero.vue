733f0cb1eab70e68319c3ee43cc16245d0139a69
Jarosław Nieściór <jaroslaw.niescior@binarapps.com>
2021-12-30 19:13:32 +0100

    feat: Hero componnet: refactor, ui fix based on figma

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,109 @@
 <template>
   <section id="hero">
     <div class="container">
-      <div class="row align-items-center">
-        <div class="col col-12 col-sm-12 col-md-7 text-left">
+      <div class="row baseline">
+        <div class="col-sm-12 col-md-7">
           <h1>
             Decentralized apps for the Cardano Blockchain
           </h1>
-          <p>Byron Network aims to bring the cutting edge solution for traders, hodlers and blockchain lovers. HD Mobile Wallet and a decentralized exchange on Cardano Blockchain is only the first step in the development of the Byron ecosystem. Our goal is to deliver investment tools and a cryptocurrency gateway for multichain investing for any blockchain enthusiast. Great experience with profound UX, thoughtful components and ability to integrate advantageous API is our top priority.</p>
+          <p>
+            Building an ecosystem that combines top-notch blockchain-based
+            products. Delivering as a team backed up with blockchain experience
+            to guarantee best user-experience.
+          </p>
+          <div class="buttons d-flex">
+            <a
+              class="btn btn-outline-primary p-2"
+              target="_blank"
+              href="/static/byron-network-lightpaper.pdf"
+              >Lightpaper</a
+            >
+            <a
+              class="btn btn-outline-primary p-2"
+              target="_blank"
+              href="/static/byron-network-whitepaper.pdf"
+              >Whitepaper</a
+            >
+          </div>
         </div>
-        <div class="col col-12 col-sm-12 col-md-5">
-          <div class="img-wrapper">
-            <img class="hero-img img-fluid" alt="" src="../assets/hero-03.png">
+        <div class="col-sm-12 col-md-5 offset-background" />
+      </div>
+      <div class="row parts">
+        <div class="col-sm-6 col-xl-3">
+          <div class="part-item">
+            <div class="part-heading">
+              <img class="part-icon" src="../assets/swap2.png" />
+              <h4>Trading options</h4>
+            </div>
+            <h5>based on the pro traders experience</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="part-item">
+            <div class="part-heading">
+              <img class="part-icon" src="../assets/wallet2.png" />
+              <h4>Economic incentives</h4>
+            </div>
+            <h5>rewards as a Performer or a Liquidity Provider</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="part-item">
+            <div class="part-heading">
+              <img class="part-icon" src="../assets/cardanoshield1.png" />
+              <h4>Order Book Pattern</h4>
+            </div>
+            <h5>resistant to price manipulation</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="part-item">
+            <div class="part-heading">
+              <img class="part-icon" src="../assets/coin1.png" />
+              <h4>Multi-chain</h4>
+            </div>
+            <h5>fully operative ecosystem on various blockchains</h5>
+          </div>
+        </div>
+      </div>
+      <div class="row features">
+        <div class="col-sm-6 col-xl-3">
+          <div class="feature-item">
+            <div class="feature-icon">
+              <img src="../assets/tradevolume.png" />
+            </div>
+            <h4>$342B</h4>
+
+            <h5>Trade Volume</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="feature-item">
+            <div class="feature-icon">
+              <img src="../assets/union.png" />
+            </div>
+            <h4>+2M</h4>
+
+            <h5>Active users</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="feature-item">
+            <div class="feature-icon">
+              <img src="../assets/money.png" />
+            </div>
+            <h4>53,324,233</h4>
+
+            <h5>All time trades</h5>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+          <div class="feature-item">
+            <div class="feature-icon">
+              <img src="../assets/line.png" />
+            </div>
+            <h4>+200</h4>
+            <h5>Financial instruments</h5>
           </div>
         </div>
       </div>
@@ -19,74 +112,178 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'Hero',
-  components: {
-  }
-}
+  components: {},
+};
 </script>
 
 <style lang="scss" scoped>
+#hero {
+  background-image: url('../assets/cardano-hero-bckg.png');
+  background-repeat: no-repeat;
+  background-position: 0 75px;
 
-  #hero {
-    .hero-dapps{
-      margin-top:  7rem!important;
-    }
-    background-image: url("../assets/cardano-hero-bckg.png");
+  .offset-background::before {
+    content: '';
+    background-image: url('../assets/Hero.png');
     background-repeat: no-repeat;
-    background-position: 0 75px;
+    background-position: top center;
+    background-size: contain;
+    width: 100%;
+    height: 600px;
+    display: block;
+    position: absolute;
+  }
 
-    h1 {
-        font-size: 42px;
-        text-align: left;
-        margin-top: 50px;
+  h1 {
+    font-size: 42px;
+    text-align: left;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 22px;
+    text-align: left;
+    color: #000;
+    font-size: 1rem;
+  }
+
+  .buttons {
+    .btn {
+      padding: 15px 40px !important;
+      border-radius: 12px;
+      font-weight: 700;
+
+      &:nth-child(2) {
+        margin-left: 12px;
       }
 
-    .hero-img {
-      min-width: 80px;
-      max-width: 550px;
-      max-height: 600px;
-    }
-
-    @media (min-width: 1200px) { 
-
-      h1 {
-        font-size: 58px;
-        text-align: left;
-        margin-top: 80px;
+      &:hover {
+        color: white;
       }
-
-      p {
-        margin-top: 22px;
-        text-align: left;
-
-      }
-
-      a.btn {
-        margin-top: 67px;
-        padding: 16px 32px;
-        border-radius: 12px;
-        margin-right: 14px;
-      }
-
-      .wrapper {
-        width: auto;
-        width: 80%;
-        padding: 0;
-        border-right: 1px solid #E2E3F4;
-      }
-        p {
-          color: #000;
-          font-size: 1rem;
-        }
-      
     }
   }
-  
 
+  .parts {
+    margin-top: 80px;
+    row-gap: 1rem;
 
+    .part-item {
+      width: fit-content;
+      margin: auto;
 
+      .part-heading {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 45px;
 
+        .part-icon {
+          margin-right: 12px;
+        }
+
+        h4 {
+          font-weight: bold;
+          font-size: 15px;
+          line-height: 26px;
+          color: #000637;
+          margin: 0;
+        }
+      }
+
+      h5 {
+        font-weight: normal;
+        font-size: 10px;
+        line-height: 26px;
+        color: #8789a8;
+        text-transform: uppercase;
+        letter-spacing: 0.1425em;
+        float: left;
+        margin: 0;
+      }
+    }
+  }
+
+  .features {
+    margin-top: 90px;
+    row-gap: 1rem;
+
+    .feature-icon {
+      height: 52px;
+      margin-bottom: 30px;
+    }
+
+    h4 {
+      font-weight: 800;
+      font-size: 30px;
+      color: #000637;
+      margin-bottom: 20px;
+    }
+
+    h5 {
+      font-weight: normal;
+      font-size: 14px;
+      color: #6d6f8d;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .offset-background {
+      height: 300px;
+      &::before {
+        width: 95%;
+        height: 100%;
+      }
+    }
+
+    .buttons {
+      justify-content: space-around;
+      margin-bottom: 10px;
+    }
+  }
+
+  @media (max-width: 991px) {
+    .baseline {
+      align-items: baseline;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    h1 {
+      font-size: 58px;
+      margin-top: 80px;
+    }
+
+    .parts {
+      & > * {
+        &:not(:first-child)::before {
+          content: '';
+          height: 55px;
+          width: 1px;
+          background: #e2e3f4;
+          left: 0%;
+          bottom: 5px;
+          position: absolute;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .parts {
+      & > * {
+        &:not(:nth-child(odd))::before {
+          content: '';
+          height: 55px;
+          width: 1px;
+          background: #e2e3f4;
+          left: 0%;
+          bottom: 5px;
+          position: absolute;
+        }
+      }
+    }
+  }
+}
 </style>
