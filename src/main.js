ae486d6ccb51f7f3262cb54b1d3b50bca38fa4ea
Jarosław Nieściór <jaroslaw.niescior@binarapps.com>
2022-01-14 01:13:22 +0100

    feat: add bootstrap icons, update AOS

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
+  render: (h) => h(App),
   mounted() {
-    AOS.init()
+    AOS.init({
+      mirror: true,
+    });
   },
-}).$mount('#app')
+}).$mount('#app');
