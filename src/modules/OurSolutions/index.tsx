93cc7e06c03762b540804eb244685d8c2fa53c35
Lucyna Borkowska <lucyna.borkowska@mail.com>
2022-10-13 16:07:21 +0200

    copy change

--- src/modules/OurSolutions/index.tsx
+++ src/modules/OurSolutions/index.tsx
@@ -117,7 +117,7 @@ const galleryItems: GalleryItemtype[] = [
       'BynetDEX is an order book DEX that brings a CEX-similar experience to the decentralized world. Advanced order types, such as limit or stop loss orders, create more flexible trading with greater risk control.',
     url: 'https://dex-ui.testnet.byron.network/',
     urlOptional: '',
-    buttonCopy: 'Join soon',
+    buttonCopy: 'Try on Testnet',
     buttonCopyOptional: ''
   },
   {
