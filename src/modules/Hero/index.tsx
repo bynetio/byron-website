5576522128ddb0bd33145e5115e58269bcae2c8b
Jakub Zagórski <70406931+zagoorland@users.noreply.github.com>
2022-10-14 10:46:29 +0200

    fixed line toggling in hero

--- src/modules/Hero/index.tsx
+++ src/modules/Hero/index.tsx
@@ -7,7 +7,8 @@ const Hero = () => (
   <div className="flex flex-col items-center px-6 md:px-12 hero-bg pb-12  pt-32 md:pt-48">
     <div className="flex flex-col max-w-[1440px]">
       <h1 className="font-['Mulish'] text-center text-4xl md:text-7xl font-bold mb-48 md:mb-32 text-byron-gray whitespace-pre-line leading-loose">
-        Building a DeFi ecosystem <br></br> with solutions for
+        Building a DeFi ecosystem <br /> with solutions for
+        <br />
         <TypeAnimation
           className="inline header-gradient"
           cursor={true}
